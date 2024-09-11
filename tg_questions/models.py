13cb29607afc89ededc16738636aa95ccd0160c6
Vergelsky <graf.werger@gmail.com>
2024-09-11 14:54:06 +0700

    Добавил опросам флаг активности и период действия

--- tg_questions/models.py
+++ tg_questions/models.py
@@ -4,9 +4,17 @@ from django.contrib.auth.models import User
 
 
 class Survey(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
-    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Создатель опроса")
+    title = models.CharField(max_length=255, verbose_name="Название")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    is_active = models.BooleanField(default=False, verbose_name="Активен")
+    is_permanent = models.BooleanField(default=False, verbose_name="Постоянный")
+    expiration_date = models.DateTimeField(blank=True, null=True, verbose_name="Срок действия")
+
+    class Meta:
+        verbose_name = "Опрос"
+        verbose_name_plural = "Опросы"
 
     def __str__(self):
         return self.title
@@ -15,7 +23,11 @@ class Survey(models.Model):
 class Question(models.Model):
     survey = models.ForeignKey(Survey, related_name='questions', on_delete=models.CASCADE)
     text = models.CharField(max_length=255)
-    order = models.PositiveIntegerField()  # для упорядочивания вопросов
+    order = models.PositiveIntegerField()
+
+    class Meta:
+        verbose_name = "Вопрос"
+        verbose_name_plural = "Вопросы"
 
     def __str__(self):
         return self.text
@@ -24,6 +36,10 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, related_name='choices', on_delete=models.CASCADE)
     text = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "Вариант ответа"
+        verbose_name_plural = "Варианты ответов"
 
     def __str__(self):
         return self.text
@@ -34,6 +50,10 @@ class Response(models.Model):
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
     completed_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name = "Ответ"
+        verbose_name_plural = "Ответы"
+
     def __str__(self):
         return f"Response by {self.user} for {self.survey}"
 
@@ -42,6 +62,10 @@ class UserResponse(models.Model):
     response = models.ForeignKey(Response, related_name='user_responses', on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Ответ пользователя"
+        verbose_name_plural = "Ответы пользователя"
 
     def __str__(self):
         return f"{self.question}: {self.choice}"
