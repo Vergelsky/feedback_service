8b06105ff38b616571e571ab171d2e384d9d2e08
Vergelsky <graf.werger@gmail.com>
2024-09-11 14:21:45 +0700

    Модели данных создал

--- tg_questions/models.py
+++ tg_questions/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Survey(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    survey = models.ForeignKey(Survey, related_name='questions', on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    order = models.PositiveIntegerField()  # для упорядочивания вопросов
+
+    def __str__(self):
+        return self.text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, related_name='choices', on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.text
+
+
+class Response(models.Model):
+    client = models.CharField(max_length=255, default="Anonymous")
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    completed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Response by {self.user} for {self.survey}"
+
+
+class UserResponse(models.Model):
+    response = models.ForeignKey(Response, related_name='user_responses', on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.question}: {self.choice}"
